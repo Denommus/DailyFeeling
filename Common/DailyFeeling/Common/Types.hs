8d6531bd006eebf22c98c0f40761d62844dc7f33
Yuri Albuquerque <yuridenommus@gmail.com>
2015-07-15 13:35:02 -0400

    Entry and Mood types

---
+++ Common/DailyFeeling/Common/Types.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE DeriveGeneric #-}
+module DailyFeeling.Common.Types (Mood(..), Entry(..)) where
+
+import GHC.Generics
+import Data.Aeson
+
+data Mood = Happy
+          | Normal
+          | Sad deriving (Generic)
+
+instance ToJSON Mood
+
+data Entry = Entry { mood :: Mood
+                   , name :: String
+                   , description :: String
+                   } deriving (Generic)
+
+instance ToJSON Entry
