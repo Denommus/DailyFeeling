bd947ce00e9c25217affe532c8a4276a579125c3
Yuri Albuquerque <yuridenommus@gmail.com>
2015-07-15 13:41:29 -0400

    Testing servant's routing

--- Backend/Main.hs
+++ Backend/Main.hs
@@ -7,8 +7,13 @@ import Network.Wai
 import Network.Wai.Handler.Warp
 import Servant
 import Paths_DailyFeeling_backend
+import DailyFeeling.Common.Types
 
 type API = "home" :> Raw
+           :<|> "entries" :> Get '[JSON] [Entry]
+
+entries :: [Entry]
+entries = [Entry Happy "Yuri" "Good day"]
 
 api :: Proxy API
 api = Proxy
@@ -18,6 +23,7 @@ raw dir = serveDirectory dir
 
 server :: FilePath -> Server API
 server dir = raw dir
+             :<|> return entries
 
 app :: FilePath -> Application
 app dir = serve api $ server dir
