f9a745c38f33302c548f470a4b454e2e0c525859
Yuri Albuquerque <yuridenommus@gmail.com>
2015-07-22 22:58:23 -0400

    Fetching and showing entries.

--- Frontend/Main.hs
+++ Frontend/Main.hs
@@ -1,4 +1,38 @@
+import Data.Time.Clock
 import Reflex.Dom
+import DailyFeeling.Common.Types
+import Control.Monad
+
+feelingInput :: MonadWidget t m => m ()
+feelingInput = do
+  return ()
+
+feelingTable :: MonadWidget t m => [Entry] -> m ()
+feelingTable feelings = do
+  el "table" $ do
+    el "tr" $ do
+      el "th" $ text "Name"
+      el "th" $ text "Mood"
+      el "th" $ text "Reason"
+
+    forM_ feelings $ \feeling -> do
+      el "tr" $ do
+        el "td" $ text $ name feeling
+        el "td" $ text $ show . mood $ feeling
+        el "td" $ text $ description feeling
+
+getFeelings :: (MonadWidget t m) => UTCTime -> m (Event t [Entry])
+getFeelings currTime = do
+  t <- tickLossy 1 currTime
+  entries <- getAndDecode $ const "/entries" <$> t
+  return $ maybe [] id <$> entries
 
 main :: IO ()
-main = mainWidget $ el "div" $ text "Hello, world!"
+main = do
+  currTime <- getCurrentTime
+  mainWidget $ do
+    feelingInput
+    entriesEvent <- getFeelings currTime
+    entries <- holdDyn [] entriesEvent
+    dyn =<< mapDyn feelingTable entries
+    return ()
